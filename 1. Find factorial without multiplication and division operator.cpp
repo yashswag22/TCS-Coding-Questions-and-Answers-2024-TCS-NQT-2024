f0d696de09810ae4459699ea2fc3c85218b00894
Gaurav kumar <102864247+yashswag22@users.noreply.github.com>
2024-08-28 17:16:37 +0530

    Update 1. Find factorial without multiplication and division operator.cpp

--- 1. Find factorial without multiplication and division operator.cpp
+++ 1. Find factorial without multiplication and division operator.cpp
@@ -1,1 +1,23 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
 
+int main() {
+    
+    int n;
+    cin>>n;
+     int ans = 1 ;
+     for(int i = 1;i<=n;i++){
+         
+         int temp = 0;
+         for(int j = 0;j<i;j++){
+             temp = temp + ans;
+         }
+         ans = temp;
+     }
+     
+     cout<<ans;
+
+    return 0;
+}
